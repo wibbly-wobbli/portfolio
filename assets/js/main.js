8fcc25617cb2267b88c2947a1b2bde119c9687bc
wibbly-wobbli <157546970+wibbly-wobbli@users.noreply.github.com>
2025-05-01 18:24:11 -0500

    refactor: update styles and structure for home and about sections in HTML, add JavaScript functionality for tab interactions

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/*--==== Show Menu ====--*/
+
+/*--==== Menu Show ====--*/
+/* Validate if constant exists */
+
+/*--==== Remove Menu Mobile ====--*/
+
+/*--==== Scroll Sections Active Link ====--*/
+
+/*--==== Change Background Header ====--*/
+
+/*--==== Show Scroll Up ====--*/
+
+/*--==== About Tabs ====--*/
+const tabs = document.querySelectorAll('[data-target]'), 
+ tabContents = document.querySelectorAll('[data-content]');  
+
+ tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dateset.target);
+        console.log(target);
+    
+    tabContents.forEach((tabContent) => {
+     tabContent.classList.remove('tab__active');
+    });
+
+    target.classList.add('tab__active');
+
+    tabs.forEach((tab) => {
+        tab.classList.remove('tab__active');
+    });
+
+    tabs.forEach((tab) => {
+        tab.classList.remove('tab__active');
+    });
+
+    tab.classList.add('tab__active');
+    });
+ });
+
+/*--==== Contact Form ====--*/
