448189a7f5169e1e8903bee87c46c5bf3c2f4505
wibbly-wobbli <157546970+wibbly-wobbli@users.noreply.github.com>
2025-05-02 10:57:09 -0500

    pushing

--- main.js
+++ main.js
@@ -13,21 +13,17 @@
 
 /*--==== About Tabs ====--*/
 const tabs = document.querySelectorAll('[data-target]'), 
- tabContents = document.querySelectorAll('[data-content]');  
-
- tabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dateset.target);
+    tabContents = document.querySelectorAll('[data-content]'); 
+ 
+    tabs.forEach((tab) => {
+     tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
     
     tabContents.forEach((tabContent) => {
      tabContent.classList.remove('tab__active');
     });
 
     target.classList.add('tab__active');
-
-    tabs.forEach((tab) => {
-        tab.classList.remove('tab__active');
-    });
 
     tabs.forEach((tab) => {
         tab.classList.remove('tab__active');
