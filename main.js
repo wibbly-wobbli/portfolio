7f804fc219f8f80cef543d96e0562edda4986ffd
wibbly-wobbli <157546970+wibbly-wobbli@users.noreply.github.com>
2025-05-05 22:22:04 -0500

    committing, still not done! But shes looking good!

--- main.js
+++ main.js
@@ -34,3 +34,54 @@ const tabs = document.querySelectorAll('[data-target]'),
  });
 
 /*--==== Contact Form ====--*/
+const contactForm = document.getElementById('contact-form'),
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+contactSubject = document.getElementById('contact-subject'),
+contactMessage = document.getElementById('contact-message'),
+errorMessage = document.getElementById('error-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // check if the field has a value
+    if (contactName.value === '' || 
+        contactEmail.value === '' || 
+        contactSubject.value === '' || 
+        contactMessage.value === ''
+    ) {
+        //show message
+        errorMessage.textContent = 'Write all the input fields';
+    }
+
+    else {
+        //serviceID - templateID - #form - publicKey
+        emailjs.sendForm(
+            'service_h80f629',
+            'template_9mjhdtd',
+            '#contact-form',
+            'zUac8CtyLmXkJpCfb'
+        ).then(() => {
+            //show message and add color, window + dot to open emoji
+            errorMessage.classList.add('color-first');
+            errorMessage.textContent = 'Message sent successfully ✅';
+
+            //remove message after 5 seconds
+            setTimeout(() => {
+                errorMessage.textContent = '';
+            }, 5000);
+        }, (error) => {
+            alert ('OOPS! Something went wrong...', error);
+        }
+    );
+
+    //clear input fields
+    contactName.value = '';
+    contactEmail.value = '';
+    contactSubject.value = '';
+    contactMessage.value = '';
+
+    }
+};
+
+contactForm.addEventListener('submit', sendEmail);
