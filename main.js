ef76478da1232e30270eb047ba4fa820a62175bc
wibbly-wobbli <157546970+wibbly-wobbli@users.noreply.github.com>
2025-05-28 16:14:11 -0500

    committing changes

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 /*--==== Show Menu ====--*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+// Show Menu
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+// Hide Menu
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+// Close menu on link click (for mobile)
+const navLinks = document.querySelectorAll('.nav__link');
+
+navLinks.forEach((n) => n.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+}));
+
 
 /*--==== Menu Show ====--*/
 /* Validate if constant exists */
